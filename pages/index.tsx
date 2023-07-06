fe316027dc336e2152616936ee91b54ac1ae008c
Mat Tejerina <matiastejerina94@gmail.com>
2023-07-06 01:58:47 -0300

    getStaticProps

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,44 @@
-import { FC } from "react"
-import { Button } from "@nextui-org/react"
 import { Layout } from "@/components/layouts"
+import { NextPage, GetStaticProps } from "next"
 
-const HomePage: FC = () => {
+const HomePage: NextPage = (props) => {
+
   return (
     <Layout>
       <div className="w-screen h-screen flex-col flex justify-center items-center">
 
-        <h1 className=''>Hola MUNDO</h1>
-        <Button name="hola" color={"primary"} ghost >HOLA</Button>
+        <ul>
+
+          <li>Pokémon</li>
+          <li>Pokémon</li>
+          <li>Pokémon</li>
+          <li>Pokémon</li>
+          <li>Pokémon</li>
+          <li>Pokémon</li>
+          <li>Pokémon</li>
+          <li>Pokémon</li>
+
+        </ul>
 
       </div>
 
     </Layout >
   )
+}
+
+// You should use getStaticProps when:
+//- The data required to render the page is available at build time ahead of a user’s request.
+//- The data comes from a headless CMS.
+//- The data can be publicly cached (not user-specific).
+//- The page must be pre-rendered (for SEO) and be very fast — getStaticProps generates HTML and JSON files, both of which can be cached by a CDN for performance.
+
+export const getStaticProps: GetStaticProps = async (ctx) => {
+
+  return {
+    props: {
+
+    }
+  }
 }
 export default HomePage
 
